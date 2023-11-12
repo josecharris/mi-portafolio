ba470a51799a7a2901263408622c79bef23a917e
josecharris <josepthc9781@gmail.com>
2023-11-12 17:50:53 -0500

    Se agrega funcionalidad de cambio de idioma

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function changeLanguage(){
+  var actualLanguage = document.getElementById("textIcon");  
+  var imagen = document.getElementById("iconLanguage");
+  if( actualLanguage.textContent === "ES"){
+    imagen.src = "assets/eng.png";
+    actualLanguage.textContent = "EN";
+  }else{
+    imagen.src = "assets/espana.png";
+    actualLanguage.textContent = "ES";
+  }
+}
